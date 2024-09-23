35ec206543a137479af623940472897792eb10d2
ONE1\pesyas <pesyas@one1.co.il>
2024-09-23 09:08:47 +0300

    add demand report for yad sarah

--- test-controller/controller/queryYadSara.js
+++ test-controller/controller/queryYadSara.js
@@ -2,7 +2,20 @@
 import logger from "../config/logger.js";
 import client from "../libs/octopusClient.js";
 import { errorE005thrower } from "../errorThrower.js";
+import fs from 'fs/promises';
 
+let stagesJsonUrl='D:\projects\api\report & email\report-examples\yad sara demand\input files\demand.json';
+
+export const readStages=async()=>{
+    try{       
+           const data =await  fs.readFile(stagesJsonUrl, 'utf-8');
+           return data?.stages || []
+    }
+    catch(err){
+        logger.error('fail to get stages json');
+        return [];
+    }
+}
 
 export const putParams = (params, jsonReport, stringReport) => {
 
@@ -17,7 +30,7 @@ export const putParams = (params, jsonReport, stringReport) => {
 
     return stringReport;
 
-}    
+}
 
 export const queryStages = async (stringReport, jsonReport) => {
     try {
@@ -43,7 +56,13 @@ const isObject = (variable) => typeof variable === 'object' && variable !== null
 export const query1stage = async (stage, stringReport, jsonReport) => {
     let result;
     if (stage.query)
-        result = await client.request('db', stage.query);
+        try {
+            result = await client.request('db', stage.query);
+        }
+        catch (err) {
+            logger.error(`fail to query stage from db: ${err.message}`);
+            result =[];
+        }
     else result = stage.value
     if (stage.breakOnEmpty && (!result || result.length == 0))
         throw { message: `Can't execute report stage: ${stage.name}. empty results for stage '${stage.name}'. `, status: 401 };
@@ -73,131 +92,10 @@ const createStagesResult = async () => {
         logger.debug('createStagesResult start!');
 
         // query db
-        const stages = [//array of stages of data
-            {
-                name: 'lgstc_actl_docs',
-                query: {
-                    select: [
-                        'LGSTC_ACTL_DOCS.PRMY_TRNS_CLSS',
-                        'LGSTC_ACTL_DOCS.EVNT_DATE',
-                        'LGSTC_ACTL_DOCS.DOC_NBR',
-                        'LGSTC_ACTL_DOCS.ORDR_NBR',
-                        'LGSTC_ACTL_DOCS.DFLT_STRM_CODE',
-                        'LGSTC_ACTL_DOCS.DFLT_TO_STRM_CODE',
-                        'LGSTC_ACTL_DOCS.TRUCK_FULL_NBR',
-                        'LGSTC_ACTL_DOCS.DOC_RMRK',
-                        'LGSTC_ACTL_DOCS.TRNS_STAT',
-                        'LGSTC_ACTL_DOCS.FIRM_CODE',
-                        'LGSTC_ACTL_DOCS.FREE_FLD_1',
-
-                        // for join tables
-
-                        // join tables field
-                        'STRMS.STRM_NAME',
-                        // 'STRMS_TO.STRM_NAME',
-                        'DRIVERS.DRIVER_NAME',
-                        'TRNS_CLSS_PARMS.MAIN_SPAWN_SECND_TRNS',
-                        'LGSTC_TRNS_TIME.LINE_NBR',
-                        'LGSTC_TRNS_TIME.CRE_DATIME'
-
-                    ],
-
-                    from: 'LGSTC_ACTL_DOCS',
-                    where: [
-                        { field: 'LGSTC_ACTL_DOCS.FIRM_CODE', value: "%{params.firm_code}%", },
-                        { field: 'LGSTC_ACTL_DOCS.DOC_NBR', value: "%{params.doc_nbr}%", },
-                        { field: 'LGSTC_ACTL_DOCS.PRMY_TRNS_CLSS', value: 331, },
-                        { field: 'LGSTC_ACTL_DOCS.TRNS_STAT', value: 50, type: '<' }
-                    ],
-                    leftJoin: [
-                        {
-                            table: 'STRMS', on: [{ key1: 'LGSTC_ACTL_DOCS.DFLT_STRM_CODE', key2: 'STRMS.STRM_CODE' },
-                            { key1: 'STRMS.FIRM_CODE', key2: 'LGSTC_ACTL_DOCS.FIRM_CODE' }]
-                        },
-                        // {
-                        //     table: 'STRMS', on: [{ key1: 'LGSTC_ACTL_DOCS.DFLT_TO_STRM_CODE', key2: 'STRMS.STRM_CODE' },
-                        //     { key1: 'STRMS.FIRM_CODE', key2: 'LGSTC_ACTL_DOCS.FIRM_CODE' }]
-                        // },
-                        {
-                            table: 'DRIVERS', on: [{ key1: 'LGSTC_ACTL_DOCS.DRIVER_CODE', key2: 'DRIVERS.DRIVER_CODE' },]
-                        },
-
-                        {
-                            table: 'TRNS_CLSS_PARMS', on: [
-                                { key1: 'LGSTC_ACTL_DOCS.FIRM_CODE', key2: 'LGSTC_ACTL_DOCS.FIRM_CODE' },
-                                { key1: 'LGSTC_ACTL_DOCS.PRMY_TRNS_CLSS', key2: "TRNS_CLSS_PARMS.PRMY_TRNS_CLSS" },
-                                { key1: 'LGSTC_ACTL_DOCS.SECND_TRNS_CLSS', key2: "TRNS_CLSS_PARMS.SECND_TRNS_CLSS" }
-                            ]
-                        },
-                        {
-                            table: 'LGSTC_TRNS_TIME', on: [
-                                { key1: 'LGSTC_ACTL_DOCS.DOC_NBR', key2: "LGSTC_TRNS_TIME.DOC_NBR" },
-                                { key1: 'LGSTC_ACTL_DOCS.FIRM_CODE', key2: 'LGSTC_TRNS_TIME.FIRM_CODE' },
-                                { key1: 'LGSTC_ACTL_DOCS.PRMY_TRNS_CLSS', key2: "LGSTC_TRNS_TIME.PRMY_TRNS_CLSS" },
-                            ]
-                        },
-
-                    ],
-                    first: true
-                },
-                breakOnEmpty: true // optional, if to break in case of no-result
-            },
-            {
-                name: 'lgstc_actl_trns',
-                query: {
-                    select: [
-
-                        'LGSTC_ACTL_TRNS.LINE_TRNS_STAT',
-                        'LGSTC_ACTL_TRNS.PART_CODE',
-                        'LGSTC_ACTL_TRNS.LINE_NBR',
-                        'LGSTC_ACTL_TRNS.QNTY_1',
-                        'LGSTC_ACTL_TRNS.FIRM_CODE',
-                        'LGSTC_ACTL_TRNS.ORDR_NBR',
-                        'LGSTC_ACTL_TRNS.ORDR_LINE_NBR',
-                        'LGSTC_ACTL_TRNS.PRMY_TRNS_CLSS',
-                        'LGSTC_ACTL_TRNS.DOC_NBR',
-                        'LGSTC_ACTL_TRNS.LOT_NBR',
-                       
-                        // function
-                        {
-                            raw: `GET_PART_DSCR("LGSTC_ACTL_TRNS"."FIRM_CODE","LGSTC_ACTL_TRNS"."PART_CODE","LGSTC_ACTL_TRNS"."PRMY_TRNS_CLSS",
-                "LGSTC_ACTL_TRNS"."DOC_NBR","LGSTC_ACTL_TRNS"."LINE_NBR",'XH','*','*') as PART_DSCR_XH`
-            },
-                {
-                    raw: `GET_PART_DSCR("LGSTC_ACTL_TRNS"."FIRM_CODE","LGSTC_ACTL_TRNS"."PART_CODE","LGSTC_ACTL_TRNS"."PRMY_TRNS_CLSS",
-        "LGSTC_ACTL_TRNS"."DOC_NBR","LGSTC_ACTL_TRNS"."LINE_NBR",'R',"LGSTC_ACTL_TRNS"."SPLR_CODE","LGSTC_ACTL_TRNS"."CUST_CODE") as PART_DSCR_R`
-    },
-
-
-                    ],
-                    from: 'LGSTC_ACTL_TRNS',
-                    where: [
-                        { field: 'LGSTC_ACTL_TRNS.FIRM_CODE', value: "%{params.firm_code}%", },
-                        { field: 'LGSTC_ACTL_TRNS.DOC_NBR', value: "%{params.doc_nbr}%", },
-                        { field: 'LGSTC_ACTL_TRNS.PRMY_TRNS_CLSS', value: 331, },
-                        { field: 'LGSTC_ACTL_TRNS.LINE_TRNS_STAT', value: 50, type: '<' },
-                    ],
-                },
-                breakOnEmpty: true // optional, if to break in case of no-result
-            },
-      
-            {
-                name: 'stream_to',
-                query: {
-                    select: ['STRM_CODE', 'STRM_NAME'],
-                    from: 'STRMS',
-                    where: [
-                        { field: 'FIRM_CODE', value: "%{params.firm_code}%", },
-                        { field: 'STRM_CODE', value: '%{lgstc_actl_docs.DFLT_TO_STRM_CODE}%' }
-                    ],
-                    first: true
-                },
-                breakOnEmpty: true // optional, if to break in case of no-result
-            },
-        ]
+        const stages = await readStages();
         const stringStages = JSON.stringify(stages);
 
-        const params = { firm_code: 21, doc_nbr: 3138, prmy_trns_clss: 331 };
+        const params = { firm_code: 21, doc_nbr: 15084, prmy_trns_clss: 332 };
 
         const string_stagesWithParams = putParams(params, stages, stringStages);
 
